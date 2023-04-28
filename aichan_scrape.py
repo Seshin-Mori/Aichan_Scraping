d3c9eeed522572d26d29dbfa37248ffdc9ed8a48
DESKTOP-1LH23A7\268ga <rap.97268@gmail.com>
2023-04-29 00:49:13 +0900

    全ての数値を一括でソートできるように修正

--- aichan_scrape.py
+++ aichan_scrape.py
@@ -4,47 +4,14 @@ from datetime import datetime, timedelta
 import pytz
 import re
 
-# 現在の日時（日本時間を取得する）
-#now = datetime.now(pytz.timezone('Asia/Tokyo'))
-
-# テスト用日時（コメントアウトを解除して使用）
-test_date = '2023-04-22'
-now = datetime.strptime(test_date, '%Y-%m-%d').replace(tzinfo=pytz.timezone('Asia/Tokyo'))
-
-formatted_date = now.strftime('%Y/%m/%Y%m%d')
-
-base_url = "https://www.aichan-engineer.com/"
-
-# XXの範囲を指定
-min_xx = 1
-max_xx = 99
-
-skipped_count = 0
-
-# 数値が入らないパターン
-target_url = f"{base_url}{formatted_date}-ai.html"
-
-response = requests.get(target_url)
-if response.status_code == 200:
-    # このループの前にこのURLを処理する（URLを処理するコードをここに追加）
-    pass
-else:
-    skipped_count += 1
-
-for i in range(min_xx, max_xx + 1):  # このループを追加
-    target_url = f"{base_url}{formatted_date}-ai_{i:02}.html"
-
+# URLの処理を行う関数
+def process_url(target_url):
     response = requests.get(target_url)
     if response.status_code != 200:
-        skipped_count += 1
-        continue
+        return []
 
-    # HTML解析
     soup = BeautifulSoup(response.text, "html.parser")
-
-    # クラス "tab" のdivをすべて見つける
     divs = soup.find_all("div", class_="tab")
-
     race_differences = []
 
     for idx, div in enumerate(divs, start=1):
@@ -58,12 +25,32 @@ for i in range(min_xx, max_xx + 1):  # このループを追加
         difference = round(tr_5_value - tr_6_value, 4)
         race_differences.append((race_name, difference))
 
-    # 差分を降順に並び替える
-    race_differences.sort(key=lambda x: x[1], reverse=True)
+    return race_differences
 
-    # 並び替えた結果を出力する
-    for race_name, difference in race_differences:
-        print(f"レース名：{race_name}")
-        print(f"差分：{difference:.4f}")
+# 日時の取得とURLの生成
+now = datetime.now(pytz.timezone('Asia/Tokyo'))
+formatted_date = now.strftime('%Y/%m/%Y%m%d')
+base_url = "https://www.aichan-engineer.com/"
+min_xx = 1
+max_xx = 99
 
-print(f"\n{skipped_count} 件のURLがスキップされました。")
+all_race_differences = []
+
+# 数値が入らないパターン
+target_url = f"{base_url}{formatted_date}-ai.html"
+all_race_differences += process_url(target_url)
+
+# 数値が入るパターン
+for i in range(min_xx, max_xx + 1):
+    target_url = f"{base_url}{formatted_date}-ai_{i:02}.html"
+    all_race_differences += process_url(target_url)
+
+# すべての要素を差分が多い順に並べ替える
+all_race_differences.sort(key=lambda x: x[1], reverse=True)
+
+# 並び替えた結果を出力する
+for race_name, difference in all_race_differences:
+    print(f"レース名：{race_name}")
+    print(f"差分：{difference:.4f}")
+
+print(f"\n{len(all_race_differences)} 件のレースが処理されました。")
